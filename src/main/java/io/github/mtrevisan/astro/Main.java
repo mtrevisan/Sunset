716495198f7979a0d94c14b775dcdd5c9d80f594
Mauro Trevisan <mauro.simioni@targatelematics.com>
2023-12-06 17:27:27 +0100

    step

--- src/main/java/io/github/mtrevisan/astro/Main.java
+++ src/main/java/io/github/mtrevisan/astro/Main.java
@@ -22,11 +22,10 @@ public class Main{
 	public static void main(final String[] args){
 		final int year = 2023;
 		final ZoneId zoneId = ZoneId.of("Europe/Rome");
-		//Padova 3 °C, Treviso 4 °C, Venezia 1015.5 hPa 4 °C, Verona 3 °C, Vicenza 3 °C
-		AtmosphericModel atmosphere = AtmosphericModel.create(1021.1, 4.);
-		//cortina: 16:56
+		final AtmosphericModel atmosphere = AtmosphericModel.create(1021.1, 4.);
+		//cortina: 16:56 (2000), 16:58 (1900-1800), 16:57 (1700), 16:56 (1600), 16:55 (1500), 16:53 (1400), 16:50 (1300), 16:47 (1200), 16:41 (1100), 16:33 (1000)
 //		final GeographicLocation location = GeographicLocation.create(46.54056257185929, 12.135682113077916, 1224.)
-		//portogruaro: 16:52
+		//portogruaro: 16:52 (2000), 16:53 (1900), 16:54 (1800-1700), 16:52 (1600), 16:51 (1500), 16:49 (1400), 16:46 (1300), 16:42 (1200), 16:37 (1100), 16:29 (1000)
 //		final GeographicLocation location = GeographicLocation.create(45.776955464421896, 12.840035227634429, 7.)
 		//16:53
 		final GeographicLocation location = GeographicLocation.create(45.714920, 12.194179, 16.)
@@ -35,7 +34,7 @@ public class Main{
 //		final DecimalFormat decimalFormatter = (DecimalFormat)NumberFormat.getNumberInstance(Locale.US);
 
 
-		ZonedDateTime winterSolstice = EarthCalculator.season(year, zoneId, Season.WINTER);
+		final ZonedDateTime winterSolstice = EarthCalculator.season(year, zoneId, Season.WINTER);
 System.out.println("ws " + DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(TimeHelper.terrestrialTimeToUniversalTime(winterSolstice)) + " UTC");
 
 //ZonedDateTime date = winterSolstice
@@ -43,8 +42,8 @@ System.out.println("ws " + DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(TimeHelp
 //SunriseResult result = SPA.calculateSunriseTransitSet(date, 45.714920, 12.194179, deltaT, SPA.Horizon.CIVIL_TWILIGHT);
 //if(result instanceof SunriseResult.RegularDay regular)
 //	System.out.println("spa " + regular.sunset());
-		EarthCalculator calculator = EarthCalculator.create(location);
-		SunlightPhase sunlightPhase = calculator.sunlightPhase(winterSolstice, Zenith.CIVIL);
+		final EarthCalculator calculator = EarthCalculator.create(location);
+		final SunlightPhase sunlightPhase = calculator.sunlightPhase(winterSolstice, Zenith.CIVIL);
 		if(sunlightPhase instanceof SunlightPhase.RegularDay event){
 			final ZonedDateTime utcSunset = TimeHelper.terrestrialTimeToUniversalTime(event.sunset());
 			System.out.println(DateTimeFormatter.ISO_LOCAL_TIME.format(utcSunset) + " UTC");
